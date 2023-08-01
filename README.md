4b550d11bb25234e3e0b24ad8b63780f7272f477
Felipe Saavedra <fisaavedrae@gmail.com>
2023-07-31 21:12:08 -0400

    final

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
 # Intro
 
-En este desafío validaremos nuestros conocimientos sobre la manipulación del DOM y las instrucciones if else if y else y operadores lógicos., se utilizó Flex para el layout y la disposición de elementos y Media Query para hacer el sitio responsivo. Desarrolló con Mobile First
-
-# Responsive Breakpoint
-
-Mobile: 575px
-Desktop: sobre 575px
+En este desafío validaremos nuestros conocimientos en los contenidos de arreglos y objetos, modificándolos y mostrándolos en el DOM.
 
 # Resolución
 
 Para desarrollar estoy usando un macbook air con una resolución de 1440x900, sin embargo inspeccioné la pagina usando una resolución de 1366 x 768 y se ve de forma correcta. Use el navegador Chrome.
+
+
+# Supuestos
+
+- En este punto del documento aclaratorio, validé que los 3 campos fueran distintos a cero y se muestra un alert con el mensaje.
+```bash
+En caso de que falte algún campo por llenar se deberá avisar al usuario con una ventana emergente. 
+```
+
+- En este punto del documento aclaratorio, 
+```bash
+3. Agrega el evento click al botón del buscador (1 punto)
+```
+
+Agregué el evento  al final del archivo JS. 
+```js
+document.getElementById("btnBuscar").addEventListener("click", buscar);
+```
 
 
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,53 +1,82 @@
+
+
+
 const propiedadesJSON = [
   {
-    name: "Casa de campo",
-    description: "Un lugar ideal para descansar de la ciudad",
+    nombre: "Casa de campo",
+    descripcion: "Un lugar ideal para descansar de la ciudad",
     src:
       "https://www.construyehogar.com/wp-content/uploads/2020/02/Dise%C3%B1o-casa-en-ladera.jpg",
-    rooms: 2,
-    m: 170
+    cuartos: 2,
+    metros: 170
   },
   {
-    name: "Casa de playa",
-    description: "Despierta tus días oyendo el oceano",
+    nombre: "Casa de playa",
+    descripcion: "Despierta tus días oyendo el oceano",
     src:
       "https://media.chvnoticias.cl/2018/12/casas-en-la-playa-en-yucatan-2712.jpg",
-    rooms: 2,
-    m: 130
+    cuartos: 2,
+    metros: 130
   },
   {
-    name: "Casa en el centro",
-    description: "Ten cerca de ti todo lo que necesitas",
+    nombre: "Casa en el centro",
+    descripcion: "Ten cerca de ti todo lo que necesitas",
     src:
       "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
-    rooms: 1,
-    m: 80
+    cuartos: 1,
+    metros: 80
   },
   {
-    name: "Casa rodante",
-    description: "Conviertete en un nómada del mundo sin salir de tu casa",
+    nombre: "Casa rodante",
+    descripcion: "Conviertete en un nómada del mundo sin salir de tu casa",
     src:
       "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
-    rooms: 1,
-    m: 6
+    cuartos: 1,
+    metros: 6
   },
   {
-    name: "Departamento",
-    description: "Desde las alturas todo se ve mejor",
+    nombre: "Departamento",
+    descripcion: "Desde las alturas todo se ve mejor",
     src:
       "https://www.adondevivir.com/noticias/wp-content/uploads/2016/08/depto-1024x546.jpg",
-    rooms: 3,
-    m: 200
+    cuartos: 3,
+    metros: 200
   },
   {
-    name: "Mansión",
-    description: "Vive una vida lujosa en la mansión de tus sueños ",
+    nombre: "Mansión",
+    descripcion: "Vive una vida lujosa en la mansión de tus sueños ",
     src:
       "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
-    rooms: 5,
-    m: 500
+    cuartos: 5,
+    metros: 500
   }
 ];
+
+function cargarPropiedades(cantidadCuartos = 0, metrosDesde = 0, metrosHasta = 0) {
+  let html = "";
+  let total = 0
+  propiedadesJSON.forEach(propiedad => {
+    if (propiedad.cuartos >= cantidadCuartos && propiedad.metros >= metrosDesde && propiedad.metros <= metrosHasta || cantidadCuartos == 0) {
+      html += `<div class="propiedad">
+                <div class="img"
+                    style="background-image: url('${propiedad.src}')">
+                </div>
+                <section>
+                    <h5>${propiedad.nombre}</h5>
+                    <div class="d-flex justify-content-between">
+                        <p>Cuartos: ${propiedad.cuartos}</p>
+                        <p>Metros: ${propiedad.metros}</p>
+                    </div>
+                    <p class="my-3">${propiedad.descripcion}</p>
+                    <button class="btn btn-info ">Ver más</button>
+                </section>
+            </div>`;
+      total++;
+    }
+  });
+  document.getElementById("total-propiedades").innerHTML = total;
+  document.getElementById("Propiedades").innerHTML = html;
+}
 
 function buscar() {
   let cantidadCuartos = document.getElementById("cantidadCuartos").value;
@@ -58,13 +87,18 @@ function buscar() {
     alert("Hay campos vacíos");
     return;
   }
+  else if (cantidadCuartos <= 0 || metrosDesde <= 0 || metrosHasta <= 0) {
+    alert("Los filtros deben ser mayores que cero");
+    return;
+  }
   else if (metrosDesde > metrosHasta) {
     alert("Rango de metros no valido");
     return;
   }
   else {
-    console.log("cantidad de cuartos: " + cantidadCuartos);
-    console.log("metros desde: " + metrosDesde);
-    console.log("metros hasta: " + metrosHasta);
+    cargarPropiedades(cantidadCuartos, metrosDesde, metrosHasta);
   }
 }
+
+cargarPropiedades();
+document.getElementById("btnBuscar").addEventListener("click", buscar); // 3. Agrega el evento click al botón del buscador
